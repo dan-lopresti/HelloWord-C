bca57bbd7bbea3ac0320607e9dc4784f33642d81
Dan LoPresti <dan.lopresti@outlook.com>
2020-03-25 17:12:34 -0400

    Added a for look

--- HelloWord-C/HelloWorld.c
+++ HelloWord-C/HelloWorld.c
@@ -2,6 +2,14 @@
 
 void main()
 {
+	int counter = 0;
+	int i = 0;
 	printf("Hello from C using Visual Studio!\n");
+	for (i = 0; i < 5; i++) {
+		printf("Counter = %i\n",counter);
+		counter++;
+	} // end for
+
+
 
 }
